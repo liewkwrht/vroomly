72039264d01a39bedf762467e949baddaf6905bd
Kavinrath <kwrhtliew@gmail.com>
2024-11-24 00:09:26 +0700

    Add files via upload

---
+++ constants/imageMap.js
@@ -0,0 +1,24 @@
+const imageMap = {
+    "Acura_NSX.png": require("../assets/cars/Acura_NSX.png"),
+    "AM_Vulcan.png": require("../assets/cars/AM_Vulcan.png"),
+    "Bugatti_Chiron.png": require("../assets/cars/Bugatti_Chiron.png"),
+    "Chevy_Camaro_18.png": require("../assets/cars/Chevy_Camaro_18.png"),
+    "Dodge_Challenger_15.png": require("../assets/cars/Dodge_Challenger_15.png"),
+    "Ferrari_LaFerrari.png": require("../assets/cars/Ferrari_LaFerrari.png"),
+    "Ford_Shelby.png": require("../assets/cars/Ford_Shelby.png"),
+    "Hennessey_Venom.png": require("../assets/cars/Hennessey_Venom.png"),
+    "Honda_NSX_R_GT.png": require("../assets/cars/Honda_NSX_R_GT.png"),
+    "Koenigsegg_One-1.png": require("../assets/cars/Koenigsegg_One-1.png"),
+    "Lamborghini_Centenario_LP_770-4.png": require("../assets/cars/Lamborghini_Centenario_LP_770-4.png"),
+    "McLaren_P1.png": require("../assets/cars/McLaren_P1.png"),
+    "Mitsubishi_Lancer_08.png": require("../assets/cars/Mitsubishi_Lancer_08.png"),
+    "Nissan_GT-R_17.png": require("../assets/cars/Nissan_GT-R_17.png"),
+    "Pagani_Huayra_16.png": require("../assets/cars/Pagani_Huayra_16.png"),
+    "Peel_P50_1962.png": require("../assets/cars/Peel_P50_1962.png"),
+    "Peugeot_207.png": require("../assets/cars/Peugeot_207.png"),
+    "Porsche_911_GT3_RS_2019.png": require("../assets/cars/Porsche_911_GT3_RS_2019.png"),
+    "Toyota_GR_Supra_Large.png": require("../assets/cars/Toyota_GR_Supra_Large.png"),
+    "Volkswagen_Type_2_De_Luxe.png": require("../assets/cars/Volkswagen_Type_2_De_Luxe.png")
+};
+
+export default imageMap;
---
+++ constants/styles.js
@@ -0,0 +1,120 @@
+import { StyleSheet } from 'react-native';
+import colors from './colors';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: colors.white,  // Using color from constants
+    },
+    title: {
+        fontFamily: 'Outfit',
+        fontSize: 36,
+        fontWeight: 'bold',
+        color: colors.darkGray, // Using color from constants
+    },
+    subtitle: {
+        fontFamily: 'Outfit',
+        fontSize: 24,
+        color: colors.darkGray,     // Using color from constants
+    },
+    content: {
+        fontFamily: 'Outfit',
+        fontSize: 16,
+        color: colors.darkGray, 
+        alignSelf: 'flex-start', 
+        marginLeft: 50,
+        marginBottom: 8          
+    },
+    buttonContainer: {
+        fontFamily: "Outfit",
+        flexDirection: 'column',  
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    bluebutton: {
+        fontFamily: "Outfit",
+        backgroundColor: colors.primary,
+        paddingVertical: 10,
+        paddingHorizontal: 1,
+        borderRadius: 30,
+        marginVertical: 10, 
+        width: 158,
+        height: 41,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    whitebutton: {
+        fontFamily: "Outfit",
+        backgroundColor: colors.white, // White background
+        borderColor: colors.primary,  // Blue border
+        borderWidth: 2,               // Border thickness
+        paddingVertical: 10,
+        paddingHorizontal: 1,
+        borderRadius: 30,             // Rounded corners
+        marginVertical: 10, 
+        width: 158,
+        height: 41,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    bluebuttonText: {
+        color: colors.primary,        // Blue text
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    whitebuttonText: {
+        color: colors.white,
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+    rentbutton: {
+        fontFamily: "Outfit",
+        backgroundColor: colors.primary,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 20,
+        justifyContent: "center", // Center text vertically
+        alignItems: "center", // Center text horizontally
+    },
+    
+    textbox: {
+        backgroundColor: colors.white, 
+        paddingVertical: 12, 
+        paddingHorizontal: 15, 
+        borderRadius: 5, 
+        width: 280, 
+        height: 41, 
+        marginBottom: 20, 
+        fontSize: 16, 
+        color: colors.darkGray, 
+        borderWidth: 1,
+        borderColor: colors.darkGray,
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 22,
+    },
+    modelView: {
+        margin: 20,
+        backgroundColor: 'white',
+        borderRadius: 20,
+        width: '90%',
+        padding: 35,
+        alignItems: 'center', // Fixed typo "certer" to "center"
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    }
+    
+});
+
+export default styles;
